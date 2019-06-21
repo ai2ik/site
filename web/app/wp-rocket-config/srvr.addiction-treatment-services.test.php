71094a1f8b2822e4ffd01729d9c90a327ee10863
aizik <isaacadamshands@me.com>
2019-06-21 15:18:55 -0400

    WPR

---
+++ web/app/wp-rocket-config/srvr.addiction-treatment-services.test.php
@@ -0,0 +1,18 @@
+<?php
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+$rocket_cookie_hash = '3c564c65bc0151c564d274d6d87a4c48';
+$rocket_logged_in_cookie = 'wordpress_logged_in_3c564c65bc0151c564d274d6d87a4c48';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_secret_cache_key = '5d0d2d629834c960517273';
+$rocket_cache_mobile = 1;
+$rocket_do_caching_mobile_files = 0;
+$rocket_cache_ssl = 0;
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(?:.+/)?embed/|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_.+|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array(
+  0 => 'q',
+);
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array();
